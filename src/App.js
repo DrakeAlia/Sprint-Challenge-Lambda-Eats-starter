08e532f4cd2556a16e404b1b3b27c25b2bb1a5da
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-04-24 09:14:13 -0700

    added components folder and two components home and form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = () => {
   return (
-    <>
+    <<Router>>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
     </>
