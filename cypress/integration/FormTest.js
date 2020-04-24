da2be61871e4e115c28f42d21d793a5e7e2e73c0
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-04-24 13:30:32 -0700

    tried testing code thru cypress

--- cypress/integration/FormTest.js
+++ cypress/integration/FormTest.js
@@ -1,6 +1,6 @@
 describe("Testing the pizza routes and form", function() {
     beforeEach(function() {
-      cy.visit("http://localhost:3002/");
+      cy.visit("http://localhost:3000/");
     });
     it("Add test to inputs and submit form", function() {
       cy
--- src/App.js
+++ src/App.js
@@ -4,26 +4,26 @@ import Home from './components/Home';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
-
 function App() {
   return (
     <Router>
-     <nav className="navbar">
-     <Link to="/">
-     <button name = 'homebutton'>Home</button>
-  </Link>
-  <br/>
-  <Link to="/form"> 
-  <button name = 'orderbutton'>Order</button>
-  </Link>
-</nav>
-<div className="App">
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/form" component={Form} />
-  </Switch>
+      <nav className="navbar">
+          <Link to="/">
+            <button name = 'homebutton'>Home</button>
+            </Link>
+          <br/>
+          <Link to="/form"> 
+          <button name = 'orderbutton'>Order</button>
+          </Link>
+      </nav>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/form" component={Form} />
+        </Switch>
       </div>
     </Router>
   );
-};
+}
+
 export default App;
